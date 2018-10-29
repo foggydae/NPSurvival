82cc7c030c07e14ad7f321bfaff34b26e5765047
ren.zuo <foggydew3@gmail.com>
2018-10-28 21:14:36 -0400

    update COXTuning

--- COXTuning.py
+++ COXTuning.py
@@ -14,7 +14,6 @@ if __name__ == '__main__':
     fe = FeatureEngineer(verbose=False)
     sources = fe.get_diseases_list()
 
-
     # load data
     train_dfs = {"pancreatitis": [], "ich": []}
     test_dfs = {"pancreatitis": [], "ich": []}
@@ -42,40 +41,46 @@ if __name__ == '__main__':
         train_dfs["ich"].append(train_df)
         test_dfs["ich"].append(test_df)
 
-
     # get the parameters
-    lambds = [0.05, 0.07, 0.09, 0.1, 0.11, 0.13, 0.15]
+    lambds = [0.05, 0.07, 0.08, 0.09, 0.1, 0.12, 0.15]
 
     concordances = {
         "pancreatitis": np.zeros(len(lambds)),
         "ich": np.zeros(len(lambds))
     }
-
     ipecs = {
         "pancreatitis": np.zeros(len(lambds)),
         "ich": np.zeros(len(lambds))
     }
 
     for dataset_type in ["pancreatitis", "ich"]:
+        cur_trains = train_dfs[dataset_type]
+        cur_tests = test_dfs[dataset_type]
+
         print("\nFor the", dataset_type, "dataset:")
         for row, lambd in enumerate(lambds):
             print("[LOG] lambd = {}".format(lambd))
 
             tmp_concordances = []
             tmp_ipecs = []
-
-            for index in range(len(train_dfs[dataset_type])):
+            for index, cur_train in enumerate(cur_trains):
                 print(index, end=" ")
+                cur_test = cur_tests[index]
+                ipec = IPEC(cur_train, g_type="All_One", t_thd=0.8, 
+                    t_step="obs", time_col='LOS', death_identifier='OUT')
+
                 model = CoxPHModel(alpha=1, lambda_=lambd)
-                model.fit(train_dfs[dataset_type][index], duration_col='LOS', event_col='OUT')
-                test_time_median_pred = model.pred_median_time(test_dfs[dataset_type][index])
+                model.fit(cur_train, duration_col='LOS', event_col='OUT')
+                test_time_median_pred = model.pred_median_time(cur_test)
+                proba_matrix = \
+                    model.pred_proba(cur_test, time=ipec.get_check_points())
+
                 concordance = evaluate_predict_result(test_time_median_pred, 
-                    test_dfs[dataset_type][index], print_result=False)
+                    cur_test, print_result=False)
+                ipec_score = ipec.calc_ipec(proba_matrix, 
+                    list(cur_test["LOS"]), list(cur_test["OUT"]))
+
                 tmp_concordances.append(concordance)
-                ipec = IPEC(train_dfs[dataset_type][index], model.pred_proba, 
-                    g_type="All_One", t_thd=0.8, t_step="obs")
-                ipec_score = ipec.avg_ipec(test_dfs[dataset_type][index], num_workers=1, 
-                    print_result=False, use_multiprocess=False)
                 tmp_ipecs.append(ipec_score)
 
             avg_concordance = np.average(tmp_concordances)
