b3839863745e6396d5f96a056aef0f2b53768256
ren.zuo <foggydew3@gmail.com>
2018-11-06 08:33:10 -0500

    new experiments for RSF and AAM

--- AAMTuning.py
+++ AAMTuning.py
@@ -12,42 +12,42 @@ if __name__ == '__main__':
     
     ## set the parameters
     coef_penalizers = [0.01, 0.03, 0.05, 0.07, 0.09, 0.11, 0.13, 0.15, 0.17, 0.19, 0.21, 0.23, 0.25, 0.27, 0.29, 0.32, 0.35, 0.38, 0.4, 0.42, 0.45, 0.48, 0.5, 0.53, 0.56, 0.6, 0.63, 0.66, 0.7, 0.73, 0.76, 0.8, 0.83, 0.86, 0.9]
-    pca_flag = False
+    pca_flags = [False, True]
     dataset_idxs = [0, 1] # 0: "pancreatitis", 1: "ich", 2: "sepsis"
-    filename = filename_generator("AAM", pca_flag, dataset_idxs)
-
     
     train_dfs, test_dfs, dataset_names = load_val_data(dataset_idxs, verbose=False)
-    concordances, ipecs = load_score_containers(dataset_names, [coef_penalizers])
 
-    for dataset_name in dataset_names:
-        print("\nFor the " + dataset_name + " dataset:")
+    for pca_flag in pca_flags:
+        for dataset_idx, dataset_name in enumerate(dataset_names):
+            filename = filename_generator("AAM", pca_flag, [dataset_idx])
+            concordances, ipecs = load_score_containers([dataset_name], [coef_penalizers])
+            print("\nFor the " + dataset_name + " dataset:")
 
-        for row, coef_penalizer in enumerate(coef_penalizers):
-            print("[LOG] coef_penalizer = {}".format(coef_penalizer))
+            for row, coef_penalizer in enumerate(coef_penalizers):
+                print("[LOG] coef_penalizer = {}".format(coef_penalizer))
 
-            tmp_concordances = []
-            tmp_ipecs = []
+                tmp_concordances = []
+                tmp_ipecs = []
 
-            for index, cur_train in enumerate(train_dfs[dataset_name]):
-                model = AalenAdditiveModel(coef_penalizer=coef_penalizer, 
-                    pca_flag=pca_flag, n_components=20)
-                model.fit(cur_train, duration_col='LOS', event_col='OUT')
-                concordance, ipec_score = \
-                    calc_scores(model, cur_train, test_dfs[dataset_name][index])
+                for index, cur_train in enumerate(train_dfs[dataset_name]):
+                    model = AalenAdditiveModel(coef_penalizer=coef_penalizer, 
+                        pca_flag=pca_flag, n_components=20)
+                    model.fit(cur_train, duration_col='LOS', event_col='OUT')
+                    concordance, ipec_score = \
+                        calc_scores(model, cur_train, test_dfs[dataset_name][index])
 
-                tmp_concordances.append(concordance)
-                tmp_ipecs.append(ipec_score)
+                    tmp_concordances.append(concordance)
+                    tmp_ipecs.append(ipec_score)
 
-            avg_concordance = np.average(tmp_concordances)
-            avg_ipec = np.average(tmp_ipecs)
-            print("[LOG] avg. concordance:", avg_concordance)
-            print("[LOG] avg. ipec:", avg_ipec)
+                avg_concordance = np.average(tmp_concordances)
+                avg_ipec = np.average(tmp_ipecs)
+                print("[LOG] avg. concordance:", avg_concordance)
+                print("[LOG] avg. ipec:", avg_ipec)
 
-            concordances[dataset_name][row] = avg_concordance
-            ipecs[dataset_name][row] = avg_ipec
+                concordances[dataset_name][row] = avg_concordance
+                ipecs[dataset_name][row] = avg_ipec
 
-            print("-------------------------------------------------------")
+                print("-------------------------------------------------------")
 
-            with open(filename, 'wb') as f:
-                pickle.dump([coef_penalizers, concordances, ipecs], f, pickle.HIGHEST_PROTOCOL)
+                with open(filename, 'wb') as f:
+                    pickle.dump([coef_penalizers, concordances, ipecs], f, pickle.HIGHEST_PROTOCOL)
--- RSFTuning.py
+++ RSFTuning.py
@@ -12,49 +12,49 @@ if __name__ == '__main__':
     
     # get the parameters
     n_trees = [50]
-    max_features = [10, 20, 40, 80, 200]
-    max_depths = [6, 10]
-    pca_flag = False
-    dataset_idxs = [2] # 0: "pancreatitis", 1: "ich", 2: "sepsis"
-    filename = filename_generator("RSF", pca_flag, dataset_idxs)
-
+    max_features = [5, 10, 20, 40, 60, 80, 120, 200]
+    max_depths = [3, 6, 9, 12]
+    pca_flags = [False, True]
+    dataset_idxs = [0, 1] # 0: "pancreatitis", 1: "ich", 2: "sepsis"
 
     train_dfs, test_dfs, dataset_names = load_val_data(dataset_idxs, verbose=True)
-    concordances, ipecs = load_score_containers(dataset_names, [n_trees, max_features, max_depths])
 
-    for dataset_name in dataset_names:
-        print("\nFor the " + dataset_name + " dataset:")
+    for pca_flag in pca_flags:
+        for dataset_idx, dataset_name in enumerate(dataset_names):
+            filename = filename_generator("RSF", pca_flag, [dataset_idx])
+            concordances, ipecs = load_score_containers([dataset_name], [n_trees, max_features, max_depths])
+            print("\nFor the " + dataset_name + " dataset:")
 
-        for dim1, n_tree in enumerate(n_trees):
-            for dim2, max_feature in enumerate(max_features):
-                for dim3, max_depth in enumerate(max_depths):
-                    print("[LOG] n_tree = {}, max_feature = {}, max_depth = {}".format(
-                        n_tree, max_feature, max_depth))
+            for dim1, n_tree in enumerate(n_trees):
+                for dim2, max_feature in enumerate(max_features):
+                    for dim3, max_depth in enumerate(max_depths):
+                        print("[LOG] n_tree = {}, max_feature = {}, max_depth = {}".format(
+                            n_tree, max_feature, max_depth))
 
-                    tmp_concordances = []
-                    tmp_ipecs = []
+                        tmp_concordances = []
+                        tmp_ipecs = []
 
-                    for index, cur_train in enumerate(train_dfs[dataset_name]):
-                        model = RandomSurvivalForest(n_trees=n_tree, 
-                            max_features=max_feature, max_depth=max_depth, 
-                            pca_flag=pca_flag, n_components=int(np.max([20, max_feature*1.2])))
-                        model.fit(cur_train, duration_col='LOS', event_col='OUT')
-                        concordance, ipec_score = \
-                            calc_scores(model, cur_train, test_dfs[dataset_name][index])
+                        for index, cur_train in enumerate(train_dfs[dataset_name]):
+                            model = RandomSurvivalForest(n_trees=n_tree, 
+                                max_features=max_feature, max_depth=max_depth, 
+                                pca_flag=pca_flag, n_components=int(np.max([20, max_feature*1.2])))
+                            model.fit(cur_train, duration_col='LOS', event_col='OUT')
+                            concordance, ipec_score = \
+                                calc_scores(model, cur_train, test_dfs[dataset_name][index])
 
-                        tmp_concordances.append(concordance)
-                        tmp_ipecs.append(ipec_score)
+                            tmp_concordances.append(concordance)
+                            tmp_ipecs.append(ipec_score)
 
-                    avg_concordance = np.average(tmp_concordances)
-                    avg_ipec = np.average(tmp_ipecs)
+                        avg_concordance = np.average(tmp_concordances)
+                        avg_ipec = np.average(tmp_ipecs)
 
-                    print("[LOG] avg. concordance:", avg_concordance)
-                    print("[LOG] avg. ipec:", avg_ipec)
+                        print("[LOG] avg. concordance:", avg_concordance)
+                        print("[LOG] avg. ipec:", avg_ipec)
 
-                    concordances[dataset_type][dim1][dim2][dim3] = avg_concordance
-                    ipecs[dataset_type][dim1][dim2][dim3] = avg_ipec
+                        concordances[dataset_type][dim1][dim2][dim3] = avg_concordance
+                        ipecs[dataset_type][dim1][dim2][dim3] = avg_ipec
 
-                    print("-------------------------------------------------------")
+                        print("-------------------------------------------------------")
 
-                    with open(filename, 'wb') as f:
-                        pickle.dump([n_tree, max_features, max_depths, concordances, ipecs], f, pickle.HIGHEST_PROTOCOL)
+                        with open(filename, 'wb') as f:
+                            pickle.dump([n_tree, max_features, max_depths, concordances, ipecs], f, pickle.HIGHEST_PROTOCOL)
